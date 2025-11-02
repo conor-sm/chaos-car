84bbe58a8bd4564e3a197b7d119f1cb9f1790251
Conor <220956160+conor-sm@users.noreply.github.com>
2025-11-02 19:47:22 +0000

    First commit of the day, hours 1 hour (today) + 30 mins (over past few days). So far, improved graphics, added points system, added won screen and any other key touches.

--- script.py
+++ script.py
@@ -7,6 +7,7 @@ pygame.init()
 running = True
 game_active = False
 game_over = False
+game_won = False
 
 eventA_bool = False
 eventB_bool = False
@@ -16,8 +17,6 @@ picked_bool = False
 cooldown_start_time = 0
 cooldown_duration = 5000
 
-event_key_complete = False
-
 pick_insult_complete = False
 
 player_key_complete = False
@@ -25,12 +24,14 @@ player_key_complete = False
 event_start_time = 0
 
 event_successful = False
+just_reset = False
 
 PLAYER_IMAGE = pygame.transform.scale(pygame.image.load("data/car.png"), (128, 192))
 EVENT_A_IMAGE = pygame.transform.scale(pygame.image.load("data/red.png"), (64, 64))
 EVENT_B_IMAGE = pygame.transform.scale(pygame.image.load("data/blue.png"), (64, 64))
 GREEN_DEBUG_IMAGE = pygame.transform.scale(pygame.image.load("data/green.png"), (64, 64))
 BACKGROUND = pygame.transform.scale(pygame.image.load("data/background.png"), (480, 640))
+QUESTION_BOX = pygame.transform.scale(pygame.image.load("data/question_box.png"), (220, 145))
 
 def pick_random_event():
     global eventA_bool, eventB_bool, picked_bool
@@ -60,25 +61,34 @@ class GameClass:
         self.background_y = 0
         self.font = pygame.font.Font("data/font.ttf", 32)
         self.small_font = pygame.font.Font("data/font.ttf", 20)
-        self.rect_seperator = pygame.Rect(0, 500, 480, 140)
+        self.smaller_font = pygame.font.Font("data/font.ttf", 15)
+        self.points = 0
 
     def menu(self):
         self.screen.fill((0, 0, 0))
         print(f"running: {running} /|\ game_active: {game_active} /|\ game_over: {game_over}")
-        self.line_1 = self.font.render("Chaos Car", True, (255, 255, 255))
-        self.line_2 = self.font.render("ENTER to begin", True, (255, 255, 255))
-        self.screen.blit(self.line_1, ((self.WIDTH - self.line_1.get_width()) // 2, (self.HEIGHT - self.line_1.get_height()) // 2 - 75))
-        self.screen.blit(self.line_2, ((self.WIDTH - self.line_2.get_width()) // 2, (self.HEIGHT - self.line_2.get_height()) // 2))
+        self.line_1 = self.small_font.render("Chaos Car", True, (255, 255, 255))
+        self.line_2 = self.small_font.render("ENTER to begin", True, (255, 255, 255))
+        self.screen.blit(self.line_1, ((self.WIDTH - self.line_1.get_width()) // 2, (self.HEIGHT - self.line_1.get_height()) // 2 - 65))
+        self.screen.blit(self.line_2, ((self.WIDTH - self.line_2.get_width()) // 2, (self.HEIGHT - self.line_2.get_height()) // 2 - 30))
     
     def game_over_function(self):
         global insult_choice, pick_insult_complete
         self.screen.fill((0, 0, 0))
+        player_class.points = 0
         if not pick_insult_complete:
             pick_insult()
-        self.line_1 = self.small_font.render(f"{insult_choice}", True, (200, 200, 200))
+        self.line_1 = self.smaller_font.render(f"{insult_choice}", True, (200, 200, 200))
         self.line_2 = self.small_font.render("ENTER to try again", True, (200, 200, 200))
-        self.screen.blit(self.line_1, ((self.WIDTH - self.line_1.get_width()) // 2, (self.HEIGHT - self.line_1.get_width()) // 2 - 40))
-        self.screen.blit(self.line_2, ((self.WIDTH - self.line_2.get_width()) // 2, (self.HEIGHT - self.line_2.get_width()) // 2 - 65))
+        self.screen.blit(self.line_1, ((self.WIDTH - self.line_1.get_width()) // 2, (self.HEIGHT - self.line_1.get_height()) // 2 - 65))
+        self.screen.blit(self.line_2, ((self.WIDTH - self.line_2.get_width()) // 2, (self.HEIGHT - self.line_2.get_height()) // 2 - 30))
+    
+    def game_won_function(self):
+        self.screen.fill((0, 0, 0))
+        self.line_1 = self.smaller_font.render("How?", True, (200, 200, 200))
+        self.line_2 = self.small_font.render("Congratulations!", True, (200, 200, 200))
+        self.screen.blit(self.line_1, ((self.WIDTH - self.line_1.get_width()) // 2, (self.HEIGHT - self.line_1.get_height()) // 2 - 65))
+        self.screen.blit(self.line_2, ((self.WIDTH - self.line_2.get_width()) // 2, (self.HEIGHT - self.line_2.get_height()) // 2 - 30))
     
     def clear_event(self):
         global eventA_bool, eventB_bool, picked_bool, event_successful, cooldown_start_time
@@ -86,29 +96,31 @@ class GameClass:
         eventB_bool = False
         picked_bool = False
         event_successful = False
-        event_key_complete = False
         cooldown_start_time = pygame.time.get_ticks()
         pygame.time.set_timer(event_A.eventA_event, 0)
         pygame.time.set_timer(event_B.eventB_event, 0)
 
     def update(self):
         self.background_y += 5
-        if self.background_y > self.WIDTH:
+        if self.background_y > self.HEIGHT:
             self.background_y = 0
 
     def draw(self):
         self.screen.blit(self.background, (0, self.background_y))
         self.screen.blit(self.background, (0, self.background_y - self.HEIGHT))
-        pygame.draw.rect(self.screen,(0, 0, 0), self.rect_seperator)
 
     def game(self):
-        global picked_bool, eventA_bool, eventB_bool
+        global picked_bool, eventA_bool, eventB_bool, game_won
         print(f"running: {running} /|\ game_active: {game_active} /|\ game_over: {game_over}")
         self.screen.fill((0, 0, 0))
         current_time = pygame.time.get_ticks()
         player_class.update()
         self.update()
         self.draw()
+        if self.points > 999:
+            game_won = True
+        self.points_text = self.font.render(f"{player_class.points}", True, (0, 0, 0))
+        self.screen.blit(self.points_text, (20, 20))
         player_class.draw()
         if not picked_bool and current_time - cooldown_start_time > 5000:
             pick_random_event()
@@ -126,9 +138,10 @@ class PlayerClass():
         self.player_key_triggered = False
         self.pressed_duration = 0
         self.pressed_required_duration = 1000
+        self.points = 0
     
     def update(self):
-        global event_key_complete, game_over, game_active, player_key_complete, event_successful
+        global game_over, game_active, player_key_complete, event_successful
         keys = pygame.key.get_pressed()
         current_time = pygame.time.get_ticks()
         if eventA_bool or eventB_bool:
@@ -144,9 +157,8 @@ class PlayerClass():
                     self.pressed_duration = current_time
                 
                 elif current_time - self.pressed_duration >= self.pressed_required_duration:
-                    self.image = GREEN_DEBUG_IMAGE
-                    event_key_complete = True
                     event_successful = True
+                    self.points +=1
                     game_class.clear_event()
                     pygame.time.set_timer(pygame.USEREVENT + 99, 500)
                     return
@@ -162,9 +174,6 @@ class PlayerClass():
             self.player_key_triggered = False
             self.pressed_duration = 0
             event_successful = False
-
-        if not event_successful:
-            self.image = PLAYER_IMAGE
         
     def draw(self):
         game_class.screen.blit(self.image, (self.player_x, self.player_y))
@@ -175,6 +184,9 @@ class EventA:
         self.image_x = (game_class.WIDTH - 64) // 2
         self.image_y = 0
         self.eventA_event = pygame.USEREVENT +1
+        self.x = 0
+        self.y = 0
+        self.text_background = QUESTION_BOX
 
     def time_set(self):
         global event_start_time
@@ -183,16 +195,12 @@ class EventA:
         event_start_time = pygame.time.get_ticks()
 
     def draw(self):
-        global event_key_complete
-        self.line_1 = game_class.small_font.render("You are being attacked!", True, (200, 200, 200))
-        self.line_2 = game_class.small_font.render("Hold E to evade!", True, (200, 200, 200))
-        self.line_3 = game_class.small_font.render("Avoided, well done", True, (200, 200, 200))
+        self.line_1 = game_class.smaller_font.render("Watch Out!", True, (200, 200, 200))
+        self.line_2 = game_class.smaller_font.render("Hold [E]", True, (200, 200, 200))
+        game_class.screen.blit(self.text_background, (125, 460))
+        game_class.screen.blit(self.line_1, ((game_class.WIDTH - self.line_1.get_width()) // 2, 510))
+        game_class.screen.blit(self.line_2, ((game_class.WIDTH - self.line_2.get_width()) // 2, 540))
         game_class.screen.blit(self.image, (self.image_x, self.image_y))
-        if not event_key_complete:
-            game_class.screen.blit(self.line_1, ((game_class.WIDTH - self.line_1.get_width()) // 2, 510))
-            game_class.screen.blit(self.line_2, ((game_class.WIDTH - self.line_2.get_width()) // 2, 540))
-        elif event_key_complete:
-            game_class.screen.blit(self.line_3, ((game_class.WIDTH - self.line_3.get_width()) // 2, 510))
 
 class EventB:
     def __init__(self):
@@ -200,23 +208,21 @@ class EventB:
         self.image_x = (game_class.WIDTH - 64) // 2
         self.image_y = 0
         self.eventB_event = pygame.USEREVENT +2
+        self.text_background = QUESTION_BOX
 
     def time_set(self):
         global event_start_time
         self.event_length = 3800
         pygame.time.set_timer(self.eventB_event, self.event_length)
         event_start_time = pygame.time.get_ticks()
+
     def draw(self):
-        global event_key_complete
-        self.line_1 = game_class.small_font.render("An enemy B has spawned!", True, (200, 200, 200))
-        self.line_2 = game_class.small_font.render("Hold E!", True, (200, 200, 200))
-        self.line_3 = game_class.small_font.render("Well Done", True, (200, 200, 200))
+        game_class.screen.blit(self.text_background, (125, 460))
+        self.line_1 = game_class.smaller_font.render("Look Out!", True, (200, 200, 200))
+        self.line_2 = game_class.smaller_font.render("Hold [E]", True, (200, 200, 200))
+        game_class.screen.blit(self.line_1, ((game_class.WIDTH - self.line_1.get_width()) // 2, 510))
+        game_class.screen.blit(self.line_2, ((game_class.WIDTH - self.line_2.get_width()) // 2, 540))
         game_class.screen.blit(self.image, (self.image_x, self.image_y))
-        if not event_key_complete:
-            game_class.screen.blit(self.line_1, ((game_class.WIDTH - self.line_1.get_width()) // 2, 510))
-            game_class.screen.blit(self.line_2, ((game_class.WIDTH - self.line_2.get_width()) // 2, 540))
-        elif event_key_complete:
-            game_class.screen.blit(self.line_3, ((game_class.WIDTH - self.line_3.get_width()) // 2, 510))
 
 game_class = GameClass()
 player_class = PlayerClass()
@@ -235,9 +241,11 @@ while running:
                 cooldown_start_time = pygame.time.get_ticks()
 
             if event.key == pygame.K_RETURN and game_over:
-                game_over = False
                 game_active = True
+                game_over = False
                 cooldown_start_time = pygame.time.get_ticks()
+                pick_insult_complete = False
+                just_reset = True
 
         if event.type == event_A.eventA_event:
             pygame.time.set_timer(event_A.eventA_event, 0)
@@ -257,14 +265,20 @@ while running:
             pygame.time.set_timer(pygame.USEREVENT + 99, 0)
             game_class.clear_event()
 
-    if not game_active:
+    if just_reset:
+        pick_insult_complete = True
+        just_reset = False
+    if game_over and not game_active:
+        game_class.game_over_function()
+
+    elif game_won:
+        game_class.game_won_function()
+
+    elif not game_active:
         game_class.menu()
 
-    if game_active:
+    elif game_active:
         game_class.game()
-
-    if game_over:
-        game_class.game_over_function()
 
     pygame.display.update()
     game_class.clock.tick(60)
